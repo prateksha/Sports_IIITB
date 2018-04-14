c11877604e6edcd1cd4465e70c7de61e955933b1
prateksha.u <prateksha.u@iiitb.org>
2018-04-15 03:35:09 +0530

    Issuing Form

---
+++ test/system/issued_items_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class IssuedItemsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit issued_items_url
+  #
+  #   assert_selector "h1", text: "IssuedItem"
+  # end
+end
